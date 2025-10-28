52d0926a8695012ce8ce131f9d36873810f4e859
Jakob Primdal <jakobprimdal@gmail.com>
2025-10-28 11:26:37 +0100

    Bare bones of GUI

--- src/main/java/dk/easv/exercise_441/gui/HelloController.java
+++ src/main/java/dk/easv/exercise_441/gui/HelloController.java
@@ -1,14 +1,39 @@
 package dk.easv.exercise_441.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView lstviewAwords;
+    @FXML
+    private ListView lstviewWords;
+    @FXML
+    private ListView lstviewNwords;
+    @FXML
+    private ListView lstviewHistory;
+    @FXML
+    private Label lblWordCount;
+    @FXML
+    private Label lblSearchResult;
+
+
+    @FXML
+    private void onBtnSearch(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBtnSearchForA(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBtnSearchForN(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBtnClearHistory(ActionEvent actionEvent) {
     }
 }
